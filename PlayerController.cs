b04a9cba59fbe74880307cb34d676989d3c056e1
Coré Ventura <core.ventura@gmail.com>
2019-10-13 14:47:37 +0100

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
 	public float speed = 15.0f;
     public bool isJumpPressed = false;
     public float jumpForce = 2.0f;
     public float maxSpeed = 50f;
     public float maxJumpForce = 10f;
     public float maxScale = 8f;
-
 	private Rigidbody rb;
 	private Vector3 moveDirection;
-
 	void Start () 
 	{
 		rb = this.GetComponent<Rigidbody> ();	
